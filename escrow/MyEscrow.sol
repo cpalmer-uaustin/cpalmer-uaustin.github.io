5702381809dc4cea8c81ec8debd87fb7d7989331
cpalmer-uaustin <cpalmer@student.uaustin.org>
2025-06-10 15:31:36 -0500

    Update MyEscrow.sol

--- escrow/MyEscrow.sol
+++ escrow/MyEscrow.sol
@@ -1,52 +1,46 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract MyEscrow {
+    address public constant tokenToEscrow = 0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359;
+    IERC20 public escrowToken = IERC20(tokenToEscrow);
 
-    // Replace this address with the token you created (or whatever token you want to use).
-    address constant tokenToEscrow = 0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359 ;
-    // This tells the contract that the address you provided is a token that can be transferred.
-    IERC20 escrowToken = IERC20(tokenToEscrow); 
     address public buyerAddress = address(0);
     address public sellerAddress = address(0);
     address public mediatorAddress = address(0);
-    uint256 public amountToEscrow = 0 ;
+    uint256 public amountToEscrow = 0;
 
-    constructor() {
-        /* This function creates the contract. */  
-    }
+    constructor() {}
 
-    function openEscrow(uint256 escrowAmount, address escrowSeller, address escrowMediator) public payable {
+    function openEscrow(address escrowSeller, address escrowMediator, uint256 escrowAmount) public {
+        require(buyerAddress == address(0), "Escrow already in use.");
 
-        require(buyerAddress==address(0), "This account is already being used.");
-
-        // Record the escrow information.
         buyerAddress = msg.sender;
         sellerAddress = escrowSeller;
         mediatorAddress = escrowMediator;
         amountToEscrow = escrowAmount;
-        // Transfer to funds from the buyer to this contract.
-        escrowToken.transferFrom(msg.sender, address(this), amountToEscrow);
+
+        require(escrowToken.transferFrom(msg.sender, address(this), amountToEscrow), "Token transfer failed");
     }
 
-    function closeEscrow(uint256 paySeller) public {
-        
-        require(msg.sender==mediatorAddress, "Only the mediator can close this account");
+    function paySeller() public {
+        require(msg.sender == mediatorAddress, "Only the mediator can release funds to seller.");
+        require(escrowToken.transfer(sellerAddress, amountToEscrow), "Payment to seller failed");
+        _resetEscrow();
+    }
 
-        escrowToken.approve(address(this), amountToEscrow);
-        if (paySeller==1) {
-            // Pay the seller (transfer funds from this contract to seller wallet).
-            escrowToken.transferFrom(address(this), sellerAddress, amountToEscrow);
-        } else {
-            // Return funds to the buyer (transfer funds from this contract to buyer wallet).
-            escrowToken.transferFrom(address(this), payable(buyerAddress), amountToEscrow);
-        }
-        // Reset the variables so that the contract can be used again.
+    function returnFunds() public {
+        require(msg.sender == mediatorAddress, "Only the mediator can return funds to buyer.");
+        require(escrowToken.transfer(buyerAddress, amountToEscrow), "Return to buyer failed");
+        _resetEscrow();
+    }
+
+    function _resetEscrow() internal {
         buyerAddress = address(0);
         sellerAddress = address(0);
         mediatorAddress = address(0);
         amountToEscrow = 0;
     }
-
-    }
+}
